2f996f804a343793efe0cdfc7e6126b91e710924
Óscar Schnake <oschnake@optimisa.cl>
2018-03-25 15:36:00 -0300

    eliminada llamada a api desde el constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -9,33 +7,5 @@ import 'rxjs/add/operator/map';
 })
 export class AppComponent {
   title = 'espectacular app';
-  //private apiUrl = 'https://address-book-demo.herokuapp.com/api/contacts';
-  private apiUrl = 'https://dog.ceo/api/breeds/list';
-  data: any = {};
 
-  constructor ( private http: Http) {
-      console.log('hola');
-      this.getContacts();
-      this.getData();
-  }
-
-  getData() {
-      let dat = this.http.get(this.apiUrl).map((res: Response) => res.json());
-
-      return dat;
-  }
-
-  getContacts () {
-      this.getData().subscribe( data => {
-          console.log(data);
-          let lista = data.message;
-          console.log(lista);
-    //      res : Array <String>;
-    /*      for ( let i = 0; i < lista.length; i ++) {
-              res.push({nombre: lista[i]});
-          }
-          console.log(res);
-          this.data = data;*/
-      })
-  }
 }
